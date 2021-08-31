911d25fb1e4de10905ed5317f2adedb8b92cfc3d
Willee164 <87996780+Willee164@users.noreply.github.com>
2021-08-31 22:43:55 +0700

    change url

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,7 +1,9 @@
-const baseUrl = "https://whatever-rest-server.herokuapp.com";
+// const baseUrl = "https://whatever-rest-server.herokuapp.com";
 // const baseUrl = "http://localhost:2000";
+const baseUrl = "http://localhost:2000" || "https://flipkart-rest-server.herokuapp.com";
 
 export const api = `${baseUrl}/api`;
+// ok
 
 export const generatePublicUrl = (fileName) => {
   return `${baseUrl}/public/${fileName}`;
